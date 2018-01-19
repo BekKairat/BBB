70e589fd6745477160d266297af4a27d27f125b0
Hakon Schutt <hakon@schutt.no>
2018-01-19 16:18:01 +0100

    added click listener for numbered buttons

--- app/src/main/java/hs/ws/no/calculator/MainActivity.java
+++ app/src/main/java/hs/ws/no/calculator/MainActivity.java
@@ -1,22 +1,14 @@
 package hs.ws.no.calculator;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn9;
-    private Button btn8;
-    private Button btn7;
-    private Button btn6;
-    private Button btn5;
-    private Button btn4;
-    private Button btn3;
-    private Button btn2;
-    private Button btn1;
-    private Button btn0;
     private Button btnComma;
     private Button btnClear;
     private ImageButton btnDivide;
@@ -25,24 +17,44 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton btnAddition;
     private ImageButton btnEquals;
     private TextView resultView;
+    private Button[] buttons = new Button[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initFields();
+
+        Resources res = getResources();
+
+        for(int i = 0; i < buttons.length; i++){
+            String id = "btn" + i;
+
+            buttons[i] = (Button) findViewById(res.getIdentifier(id, "id", getPackageName()));
+        }
+
+        for (int i = 0; i < buttons.length; i++){
+            buttons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    changeViewText(view.getResources().getResourceName(view.getId()));
+                }
+            });
+        }
+
+        btnComma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String result = resultView.getText().toString();
+                if( ! result.contains(",")){
+                    resultView.append(",");
+                }
+            }
+        });
     }
 
     private void initFields(){
-        btn9 = (Button) findViewById(R.id.btn9);
-        btn8 = (Button) findViewById(R.id.btn8);
-        btn7 = (Button) findViewById(R.id.btn7);
-        btn6 = (Button) findViewById(R.id.btn6);
-        btn5 = (Button) findViewById(R.id.btn5);
-        btn4 = (Button) findViewById(R.id.btn4);
-        btn3 = (Button) findViewById(R.id.btn3);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn0 = (Button) findViewById(R.id.btn0);
         btnComma = (Button) findViewById(R.id.btnComma);
         btnClear = (Button) findViewById(R.id.btnClear);
         btnDivide = (ImageButton) findViewById(R.id.btnDivide);
@@ -53,19 +65,34 @@ public class MainActivity extends AppCompatActivity {
         resultView = (TextView) findViewById(R.id.resultView);
     }
 
-    public double divide(double x, double y){
+    private void changeViewText(String id){
+        float total = Float.parseFloat(resultView.getText().toString());
+        id = String.valueOf(id.charAt(id.length() - 1));
+
+        appeandTextResult(id, total);
+    }
+
+    private void appeandTextResult(String id, float number){
+        if (number == 0) {
+            resultView.setText(id);
+        } else {
+            resultView.append(id);
+        }
+    }
+
+    public float divide(float x, float y){
         return x / y;
     }
 
-    public double mulitply(double x, double y){
+    public float mulitply(float x, float y){
         return x * y;
     }
 
-    public double subtract(double x, double y){
+    public float subtract(float x, float y){
         return x - y;
     }
 
-    public double addition(double x, double y){
+    public float addition(float x, float y){
         return x + y;
     }
 }
